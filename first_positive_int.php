e2b7f1449e83c05233d8dff01da51ad342c5f361
nshukla <neerajshuklamrt@gmail.com>
2022-08-04 18:43:18 +0100

    Create first_positive_int.php

---
+++ first_positive_int.php
@@ -0,0 +1,14 @@
+<?php
+// Enter your code here, enjoy!
+$array = array(7,2, 1, 3, 8, 5);
+$s = sort($array);
+$fin = 0;
+for( $i = 0; $i<count($array); $i++ ){
+    if($array[$i]>=0){
+    	if(($array[$i+1]-$array[$i])>1){
+    		$fin = $array[$i]+1;
+    		break;
+    	}
+    }
+}
+echo $fin;
